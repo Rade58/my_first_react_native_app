6596f6cafdf531f51cdf0562a5b52a47188f3563
rade <bajic.rade2@gmail.com>
2020-06-24 19:03:40 +0200

    abstracting out color item list

---
+++ screens/ColorPalette.tsx
@@ -0,0 +1,148 @@
+import React, { FunctionComponent } from 'react';
+import {
+  SafeAreaView,
+  FlatList,
+  View,
+  Text,
+  StyleSheet,
+  Platform,
+} from 'react-native';
+
+import ItemBox from '../components/ItemBox';
+
+const COLORS = [
+  { colorName: 'Base03', hexCode: '#002b36' },
+  { colorName: 'Base02', hexCode: '#073642' },
+  { colorName: 'Base01', hexCode: '#586e75' },
+  { colorName: 'Base00', hexCode: '#657b83' },
+  { colorName: 'Base0', hexCode: '#839496' },
+  { colorName: 'Base1', hexCode: '#93a1a1' },
+  { colorName: 'Base2', hexCode: '#eee8d5' },
+  { colorName: 'Base3', hexCode: '#fdf6e3' },
+  { colorName: 'Yellow', hexCode: '#b58900' },
+  { colorName: 'Orange', hexCode: '#cb4b16' },
+  { colorName: 'Red', hexCode: '#dc322f' },
+  { colorName: 'Magenta', hexCode: '#d33682' },
+  { colorName: 'Violet', hexCode: '#6c71c4' },
+  { colorName: 'Blue', hexCode: '#268bd2' },
+  { colorName: 'Cyan', hexCode: '#2aa198' },
+  { colorName: 'Green', hexCode: '#859900' },
+];
+
+const ColorPalette: FunctionComponent = () => {
+  const {
+    /* first,
+    third,
+    fourth, */
+    droidSafeArea,
+    /*  second,
+    otherStyles,
+    //
+    textOne,
+    textTwo,
+    textThree,
+    textFour, */
+    //
+    explain,
+    textExplain,
+    flatListBox,
+  } = globalStyles;
+
+  return (
+    <SafeAreaView style={droidSafeArea}>
+      <View style={explain}>
+        <Text style={textExplain}>
+          Evo ih neki element i stilizovani su kao što vidiš, i nalaze se u flat
+          listi
+        </Text>
+      </View>
+      {/* --------------------------*/}
+      <Text>Fake Header</Text>
+      <FlatList
+        style={flatListBox}
+        data={COLORS}
+        keyExtractor={(item) => item.hexCode}
+        renderItem={({ item }) => (
+          <ItemBox itemName={item.colorName} boxColor={item.hexCode} />
+        )}
+        ListHeaderComponent={<Text>Neki Header</Text>}
+        ListFooterComponent={<Text>Neki Footer</Text>}
+        ListEmptyComponent={<Text>Buyaaaaaaa</Text>}
+        // horizontal={true}
+      />
+      <Text>Fake Footer</Text>
+      {/* === !== === !== === !== */}
+    </SafeAreaView>
+  );
+};
+
+export const globalStyles = StyleSheet.create({
+  flatListBox: {
+    borderColor: 'crimson',
+    borderWidth: 2,
+    marginHorizontal: 8,
+  },
+
+  explain: {
+    margin: 28,
+  },
+  textExplain: {
+    fontSize: 20,
+  },
+  // === !== ===
+  otherStyles: {
+    flex: 0,
+    borderWidth: 2,
+    paddingTop: 12,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderLeftColor: 'yellow',
+    marginTop: 18,
+    marginHorizontal: 14,
+    padding: 24,
+  },
+  // === !== === !==
+  first: {
+    backgroundColor: 'olive',
+  },
+  second: {
+    backgroundColor: 'crimson',
+  },
+  fourth: {
+    backgroundColor: 'tomato',
+  },
+  // === !== === !==
+  third: {
+    backgroundColor: 'teal',
+  },
+  // === !== === !==
+  droidSafeArea: {
+    flex: 1,
+    paddingTop: Platform.OS === 'android' ? 25 : 0,
+  },
+  // === !== === !==
+  textOne: {
+    color: 'blanchedalmond',
+    fontSize: 12,
+    fontWeight: 'normal',
+  },
+  textTwo: {
+    color: 'white',
+    fontSize: 14,
+    fontWeight: 'bold',
+    fontStyle: 'italic',
+  },
+  textThree: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: '400',
+  },
+  textFour: {
+    color: 'green',
+    fontSize: 18,
+    fontWeight: '100',
+    fontStyle: 'italic',
+  },
+});
+
+export default ColorPalette;
---
+++ screens/Home.tsx
@@ -0,0 +1,10 @@
+import React, { FunctionComponent } from 'react';
+import { Text, View } from 'react-native';
+
+const Home: FunctionComponent = () => (
+  <View>
+    <Text>tekst</Text>
+  </View>
+);
+
+export default Home;
