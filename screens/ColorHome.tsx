24980ea898465eb60a01ed8da54b0f9a04f554cf
rade <bajic.rade2@gmail.com>
2020-06-28 16:20:14 +0200

    network request example

--- screens/ColorHome.tsx
+++ screens/ColorHome.tsx
@@ -1,4 +1,10 @@
-import React, { FunctionComponent } from 'react';
+import React, {
+  FunctionComponent,
+  // UVOZIM HOOK-OVE
+  useState,
+  useCallback,
+  useEffect,
+} from 'react';
 
 import {
   View,
@@ -8,54 +14,78 @@ import {
   StyleSheet,
 } from 'react-native';
 
+// HAJDE DA PRVO NAPRAVIM INTERFACE KOJIM CU TYPE-OVATI DATA, ZA KOJI MCU PRAVITI REQUEST
+interface ApiDataItem {
+  id: number;
+  paletteName: string;
+  colors: { colorName: string; hexCode: string }[];
+}
+
+type ApiDataType = ApiDataItem[];
+
 // TYPE-OVI
-import { HomeScreenProps } from '../navigators/color-app-stack-navigator'; // ZASTO SAM UVEZAO IMENA JESTE ZATO STO SAM ODLUCIO DA KOSRISTIM SECTION LIST, A ONA NIJE DOBRO TYPED
-//                                                                              A MENI TREBAJU COLOR SCREEN NAMES
+import { HomeScreenProps } from '../navigators/color-app-stack-navigator';
+import { FlatList } from 'react-native-gesture-handler';
 
 const Home: FunctionComponent<HomeScreenProps> = ({ navigation, route }) => {
   const { params, name } = route;
-
+  // ***************************************************************************************************
+  // ***************************************************************************************************
+  // OVO VISE NECU KORISTITI, ALI MRZI ME DA VRSIM REDEFINISANJE OVOGA
+  // === !== ===--------------  NEKA OVO OSTANE ** NO-OP ** ---------------   !== === !==
   const { allColorData } = params;
 
   const pickedColorData: typeof allColorData = [];
 
-  for (let colorsObject of allColorData) {
+  /* for (let colorsObject of allColorData) {
     const { data, imeScreena } = colorsObject;
 
     pickedColorData.push({ imeScreena, data: data.slice(0, 4) });
-  }
+  } */
+
+  // ***************************************************************************************************
+  // ***************************************************************************************************
+
+  // OVDE CU CUVATI DATA I MOGU DA GA TYPE-UJEM
+  const [colorData, setColorData] = useState<ApiDataType>([]);
+
+  // ISKORISTICU USE CALLBACK U KOJEM CU DEFINISATI NETWORK REQUEST
+
+  const fetchApiDataCallback = useCallback(async () => {
+    const jsonData = await fetch(
+      'https://color-palette-api.kadikraman.now.sh/palettes'
+    );
+
+    const data: ApiDataType = await jsonData.json();
+
+    setColorData(data);
+  }, []);
+
+  useEffect(() => {
+    fetchApiDataCallback();
+  }, [fetchApiDataCallback]);
 
   return (
     <View>
       <Text>{name}</Text>
-      <SectionList
-        sections={pickedColorData}
-        // horizontal={true}
-        // OVDE JEDINO STO U SLEDECOJ FUNKCIJI RESTRUCTURED     section   NIJE TYPED I NE ZNA SE DA POSTOJI imeScreen-A
-        // PROPERTI NA NJEMU (SAMO SE data UZIMA U OBZIR, JER ONO MORA BITI TAMO)
-        // MEDJUTIM BITNO JE DA NE JAVLJA GRESKU
-        renderSectionHeader={(
-          { section: { data, imeScreena } } // ALI NISTA TE NE SPRECAVA DA RESTRUKTURIRANJEM STAVIS I     imeScreen-A
-        ) => (
+      {/* UMESTO SECTION LIST ZELIM DA KORISTIM FLAT LIST */}
+      <FlatList
+        data={colorData}
+        renderItem={({ item: { colors, paletteName } }) => (
           <TouchableOpacity
             onPress={() => {
-              // ALI NISTA MI NE SMETA DA UPOTREBIM imeScreen-A
-
               navigation.navigate('ColorPallete', {
-                colors: data.concat([]),
-                imeScreena: imeScreena,
+                colors,
+                imeScreena: paletteName,
               });
             }}
           >
-            <Text>{imeScreena}</Text>
+            <View style={[styles.colorItems]}>
+              <Text>{paletteName}</Text>
+            </View>
           </TouchableOpacity>
         )}
-        keyExtractor={(item) => item.hexCode}
-        renderItem={({ item }) => (
-          <View style={[{ backgroundColor: item.hexCode }, styles.colorItems]}>
-            <Text>{item.colorName}</Text>
-          </View>
-        )}
+        keyExtractor={(item) => item.id.toString()}
       />
     </View>
   );
